bad1b1b3e0e42adabed1a82a379a94d9b97e99bb
maichomper <maichomper@gmail.com>
2015-05-11 20:04:33 -0500

    web services for chat

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -23,7 +23,7 @@ class Api extends REST_Controller
             $this->response($error, HTTP_NOT_FOUND);
         }
 
-    } // get_chat_messages_get
+    } // get_chat_messages_post
 
     /** 
      * Returns a conversation for a given stream.
@@ -34,7 +34,7 @@ class Api extends REST_Controller
         if( ! $this->post( 'stream' ) ){
             $error = array(
                 'error'            => TRUE,
-                'responseMessage'   => "HTTP error 404. The conversation couldn't be retrieved.",
+                'responseMessage'   => "HTTP error 400 - The conversation couldn't be retrieved because not stream was given.",
             );
             $this->response($error, HTTP_BAD_REQUEST);
         }
@@ -55,7 +55,71 @@ class Api extends REST_Controller
                 break;
         }
 
-    } // get_chat_messages_get
+    } // get_chat_conversation_post
+
+    /** 
+     * Adds a message to a chat conversation.
+     */ 
+    function add_message_post() 
+    {
+
+        if( ! $this->post( 'stream' ) || ! $this->post( 'text_message' ) || ! $this->post( 'from_user_id' ) || ! $this->post( 'to_user_id' ) ){
+            $error = array(
+                'error'             => TRUE,
+                'responseMessage'   => "HTTP error 400 - The message couldn't be added because an argument was missing.",
+            );
+            $this->response($error, HTTP_BAD_REQUEST);
+        }
+
+        $this->load->model('chat_message');
+        $addedMessage = $this->chat_message->addMessage( $this->post( 'stream' ), $this->post( 'text_message' ), $this->post( 'from_user_id' ), $this->post( 'to_user_id' ) );
+        switch ( $addedMessage['error'] ) {
+            case true:
+            case false:
+                $this->response( $addedMessage, HTTP_OK );
+                break;
+            default:
+                $error = array(
+                    'error'             => TRUE,
+                    'responseMessage'   => "HTTP error 404. The message couldn't be added.",
+                );
+                $this->response( $error, HTTP_NOT_FOUND );
+                break;
+        }
+
+    } // add_message_post
+
+    /** 
+     * Delete a message from a conversation
+     */ 
+    function delete_message_post() 
+    {
+
+        if( ! $this->post( 'id' ) ){
+            $error = array(
+                'error'             => TRUE,
+                'responseMessage'   => "HTTP error 400 - The message couldn't be deleted because an argument was missing.",
+            );
+            $this->response($error, HTTP_BAD_REQUEST);
+        }
+
+        $this->load->model('chat_message');
+        $deleted = $this->chat_message->deleteMessage( $this->post( 'id' ) );
+        switch ( $deleted['error'] ) {
+            case true:
+            case false:
+                $this->response( $deleted, HTTP_OK );
+                break;
+            default:
+                $error = array(
+                    'error'             => TRUE,
+                    'responseMessage'   => "HTTP error 404. The message couldn't be deleted.",
+                );
+                $this->response( $error, HTTP_NOT_FOUND );
+                break;
+        }
+
+    } // delete_message_post
 
 
 } // class Api
--- application/models/chat_message.php
+++ application/models/chat_message.php
@@ -33,7 +33,7 @@ class Chat_message extends CI_Model {
 	} // getMessages
 
 	/**
-	 * Returns all messages of a conversation between two users.
+	 * Returns all messages of a chat conversation between two users.
 	 * @param string $stream - A unique identifier for a chat
 	 * @return mixed array $chatMessages
 	 */
@@ -64,34 +64,50 @@ class Chat_message extends CI_Model {
 	} // getConversation
 
 	/**
-	 * Returns a chat conversation between two users.
-	 * @param string $stream - A unique identifier for a chat
-	 * @return mixed array $chatMessages
+	 * Adds a new text message to an existing chat conversation.
+	 * @param string $stream - A unique identifier for a chat conversation
+	 * @param string $text_message 
+	 * @param string $from_user_id 
+	 * @param string $to_user_id 
+	 * @return mixed array $insertMessageStatus
 	 */
-	public function addMessage( $stream ){
-		$chatMessages = array();
+	public function addMessage( $stream, $text_message, $from_user_id, $to_user_id ){
+		$insertMessageStatus = array();
 
-		$conversationQuery = $this->db->get_where( 'chat_messages', array( 'stream' => $stream ) );
-		$messagesResult = $conversationQuery->result();
+		$messageData = array(
+			'stream'		=> $stream,
+			'text_message'	=> $text_message,		
+			'from_user_id'	=> $from_user_id,	
+			'to_user_id'	=> $to_user_id,	
+			);
+		$this->db->insert( 'chat_messages', $messageData );
 
-		if( count( $messagesResult ) < 1 ) {
-			$chatMessages['error'] = TRUE;
-			$chatMessages['responseMessage'] = "No conversation was found for stream: '$stream'";
-			return $chatMessages;
+		if ( $this->db->affected_rows() > 0 ){
+			$insertMessageStatus['data']['id'] = $this->db->insert_id();
+			$insertMessageStatus['error'] = FALSE;
+			$insertMessageStatus['responseMessage'] = "Text message '$text_message' added successfully";
+			return $insertMessageStatus;
 		}
 
-		foreach ( $messagesResult as $key => $messageRow )
-		{
-		    $chatMessages['data'][$key] = array(
-		    	'text_message' 	=> $messageRow->text_message,
-		    	'from_user_id' 	=> $messageRow->from_user_id,
-		    	'to_user_id' 	=> $messageRow->to_user_id,
-		    	);
-		}
-
-		$chatMessages['error'] = FALSE;
-		$chatMessages['responseMessage'] = 'Conversation retrieved successfully!';
-		return $chatMessages;
+		$insertMessageStatus['error'] = TRUE;
+		$insertMessageStatus['responseMessage'] = "The text message '$text_message' couldn't be added";
+		return $insertMessageStatus;
 	} // addMessage
-	
+
+	/**
+	 * Deletes a message from an existing conversation.
+	 * @param string $id - Message to be deleted
+	 * @return mixed array $deleteMessageStatus
+	 */
+	public function deleteMessage( $text_message_id ){
+		$deleteMessageStatus = array();
+
+		$this->db->delete( 'chat_messages', array( 'id' => $text_message_id ) );
+		$deleteMessageStatus['error'] = TRUE;
+		$deleteMessageStatus['responseMessage'] = "The text message has been deleted.";
+
+		return $deleteMessageStatus;
+
+	} // deleteMessage
+
 }
